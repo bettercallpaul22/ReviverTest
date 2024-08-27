c7cb18bd22bcb2cd0de30730203c257bf3cc06c1
obaro paul <obaroiskool@gmail.com>
2024-08-27 09:58:26 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Home from './pages/home/Home';
+import React from "react";
+import { RouterProvider } from "react-router-dom";
+import "./App.scss";
+import Home from "./pages/home/Home";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import { createBrowserRouter } from "react-router-dom";
+import DrawerAppBar from "./components/Navbar/AppBar";
+import SearchAppBar from "./components/Navbar/AppBar";
+import Project from "./pages/project/Project";
 
-function App() {
-  return (
-    <div className="">
-    <Home/>
+const App = () => {
+  const Layout = () => {
+    return (
+      <div className="main">
+        <SearchAppBar />
+        <Home />
+        <Footer />
+      </div>
+    );
+  };
 
-    </div>
-  );
-}
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/home",
+          element: <Home/>
+        },
+        {
+          path: "/project",
+          element: <Project/>
+        },
+      ],
+    },
+
+    // {
+    //   path: "/project",
+    //   element: <Project/>
+    // },
+  ]);
+
+  return <RouterProvider router={router} />;
+};
 
 export default App;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,185 +1,28 @@
-import React, { useEffect, useState } from "react";
-import "./Home.css";
-import axios from "axios";
-
-
-interface Photo {
-  url: string;
-  user: number;
-  title: string;
-  id: number;
-  description: string;
-}
+import React from 'react'
+import "./Home.scss"
+import SideMenu from '../../components/sidemenu/SideMenu'
+import Content from '../../components/body/Content'
+import RightSideMenu from '../../components/rightsidemenu/RightSideMenu'
+import useScreenSize from '../../helper/screen_size'
 
 const Home = () => {
-  const [selectedImageArr, setSelectedImageArr] = useState<number[]>([]);
-  const [isloading, setisloading] = useState(false);
-  const [imageId, setimageId] = useState(0);
-  const [photosData, setPhotosData] = useState<Photo[] | null>(null);
-  const [error, seterror] = useState("");
-  const [imageName, setimageName] = useState("");
-  const [showModal, setShowModal] = useState<boolean>(false);
-  const [name, setname] = useState("");
-  const [imageUrl, setImageUrl] = useState("");
 
-  const handleShowModal = () => setShowModal(true);
-  const handleCloseModal = () =>{ 
-    setname("")
-    setShowModal(false);
-  }
+  const { width, height } = useScreenSize();
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  const isMobile = width < 768; // Mobile devices (width less than 768px)
+  const isTablet = width >= 768 && width <= 1024; // Tablet devices (width between 768px and 1024px)
+  const isDesktop = width > 1024; // Desktop devices (width greater than 1024px)
 
-  async function fetchData() {
-    setSelectedImageArr([]);
-    setisloading(true);
-    const url =
-      "https://api.slingacademy.com/v1/sample-data/photos?offset=5&limit=100";
-
-    try {
-      const response = await axios.get(url);
-      const shuffled = response.data.photos.sort(() => 0.5 - Math.random());
-      setPhotosData(shuffled.slice(0, 4));
-
-      setisloading(false);
-    } catch (error: any) {
-      seterror(error.message);
-      setisloading(false);
-    }
-  }
-
-  const handleDownload = async () => {
-    console.log("handleDownload", imageUrl)
-    try {
-      const response = await fetch(imageUrl); 
-      const buffer = await response.arrayBuffer();
-      const url = window.URL.createObjectURL(new Blob([buffer]));
-      const link = document.createElement('a');
-      link.href = url;
-      link.download = 'image.png';
-      link.click();
-      window.URL.revokeObjectURL(url); 
-    } catch (error) {
-      console.error('Error downloading image:', error);
-    }
-  };
+  const isMobileOrTablet = isMobile || isTablet; // True if the device is mobile or tablet
+  const isTabletOrDesktop = isDesktop || isTablet; // True if the device is mobile or tablet
 
   return (
-    <div className="home">
-      <div className="main-container">
-        <div className="image-header">
-          <p>Click to generate random images</p>
-          <button className="btn" onClick={fetchData}>
-            Refresh
-          </button>
-        </div>
-        <div>
-          {photosData == null ? (
-            <div className="null-div">
-              {isloading ? <div className="spinner"></div> : <div>{error}</div>}
-            </div>
-          ) : (
-            <div className="image-container">
-              {photosData?.map(({ id, url, title }) => (
-                <div
-                  key={id}
-                  className="grid-item"
-                  onClick={() => {
-                    const newSelectedImageArr = [...selectedImageArr];
-                    newSelectedImageArr.shift();
-                    newSelectedImageArr.push(id);
-                    setSelectedImageArr(newSelectedImageArr);
-                    setimageId(newSelectedImageArr[0]); // correct this line
-                    setimageName(title);
-                    setImageUrl(url);
-                  }}
-                >
-                  {photosData === null ? (
-                    <div className="null-div">null</div>
-                  ) : (
-                    <div>
-                      <img className="img" src={url} alt="bag image" />
-                      <input
-                        checked={id === imageId}
-                        className="checkbox"
-                        type="checkbox"
-                        value={name}
-                      />
-                    </div>
-                  )}
-                </div>
-              ))}
-            </div>
-          )}
-        </div>
-
-   
-        <div className="form-style">
-          {selectedImageArr.length === 0 ? (
-            <p className="info">Please select an image to continue</p>
-          ) : (
-            <p className="info-success">Image selected success</p>
-          )}
-          <div className="input-container-style">
-            <label className="label-style">Fullname</label>
-            <input
-              onChange={(e) => {
-                setname(e.target.value);
-              }}
-              className="input"
-              type="text"
-            />
-          </div>
-          {name == "" && (
-            <p className="info">
-              {selectedImageArr.length > 0 ? "Please enter your name" : null}
-            </p>
-          )}
-
-          <button
-            disabled={selectedImageArr.length === 0 || name == ""}
-            onClick={() => {
-              
-              handleShowModal();
-           
-              // handleDownload()
-            }}
-            className="btn"
-          >
-            Submit
-          </button>
-        </div>
-      </div>
-      {showModal && (
-        <div className="modal-overlay">
-         
-          <div className="modal">
-          
-            <h2>Success!</h2>
-            <p>Your action was successful.</p>
-            <div className="img-container" style={{
-              width: '100%',
-              maxWidth: '400px',
-              backgroundImage: `url(${imageUrl})`,
-               backgroundSize: 'cover',
-               backgroundPosition: 'center',
-               backgroundRepeat: 'no-repeat',
-            }}>
-            <span>Thank you</span>
-              
-              <span>{name}</span>
-            </div>
-            <button onClick={handleCloseModal}>Dismiss</button>
-          
-
-          </div>
-        </div>
-      )}
+    <div className={isMobileOrTablet ?'home2' : 'home'}>
+     {isDesktop && (<SideMenu/>)}
+      <Content/>
+     {isDesktop && (<RightSideMenu/>)}
     </div>
-    // </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
